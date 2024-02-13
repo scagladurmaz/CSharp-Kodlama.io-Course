ea7b2ab877ede253b234ae96c674570acecd8e7f
Çağla <scagladurmaz@gmail.com>
2024-02-13 23:35:01 +0300

    2.Day- Codes-00

---
+++ intro/Entities/BaseCustomer.cs
@@ -0,0 +1,24 @@
+﻿namespace intro.Entities
+{
+    public class BaseCustomer
+    {
+        public int Id { get; set; }
+        public string CustomerNumber { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- intro/Program.cs
+++ intro/Program.cs
@@ -77,3 +77,31 @@ for (int i= 0; i< courses2.Length; i++)
 }
 
 Console.WriteLine("kod bitti");
+
+
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678922";
+customer1.FirstName = "Çağla";
+customer1.LastName = "Durmaz";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "12345678912";
+customer2.FirstName = "Özgü";
+customer2.LastName = "Durmaz";
+customer2.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name="Kodlamaio";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "12345678987";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 3;
+customer4.Name = "Abc";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "12345678985";
